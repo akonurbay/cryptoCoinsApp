8b7dc1805f3452631a386c97628d64fbc58b2808
arsen konurbay <akonurbay@mail.ru>
2025-09-25 16:56:24 +0500

    import a Equatable and add his props to the events and states , now this will check with condition that old data will check for update with new state and will restart it

--- cryptocoins_list/lib/repositories/crypto_coins/models/crypto_coin.dart
+++ cryptocoins_list/lib/repositories/crypto_coins/models/crypto_coin.dart
@@ -1,9 +1,19 @@
 // hello
 
-class CryptoCoin {
-  const CryptoCoin({required this.name, required this.priceInUSD, required this.imageUrl});
+import 'package:equatable/equatable.dart';
+
+class CryptoCoin extends Equatable {
+  const CryptoCoin({
+    required this.name,
+    required this.priceInUSD,
+    required this.imageUrl,
+  });
 
   final String name;
   final double priceInUSD;
   final String imageUrl;
+  
+  @override
+  // TODO: implement props
+  List<Object?> get props => [name, priceInUSD, imageUrl];
 }
